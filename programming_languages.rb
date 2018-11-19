43c257dc2b47cfea13816c25bfc765523fd67672
Stephen Argentina <stephena9871@gmail.com>
2018-11-19 00:30:26 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(languages)
   hash = {}
   languages.each{|key, value|
     value.each{|key1, value1|
-    if key1 in hash
+    if hash[key1]
       hash[key1][:style].push(key)
     else
       value1.each{|key2, value2|
